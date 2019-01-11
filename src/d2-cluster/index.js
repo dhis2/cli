e7507bb2a70a555d9663f15bd934f5da9387e23a
Austin McGee <>
2019-01-11 12:30:15 +0000

    TASK: Remove comments

--- src/d2-cluster/index.js
+++ src/d2-cluster/index.js
@@ -3,7 +3,6 @@ const { namespace } = require('../util/cliUtils');
 const command = namespace("cluster", {
   desc: "Manage DHIS2 Docker clusters",
   aliases: 'c',
-  // commands: [up, init, down],
   builder: yargs => {
     yargs.commandDir("commands");
   }
--- src/d2/commands/app.js
+++ src/d2/commands/app.js
@@ -7,5 +7,4 @@ module.exports = namespace("app", {
     require('./app/create'),
     require('./app/scripts'),
   ]
-  // builder: yargs => yargs.commandDir("app"),
 });
