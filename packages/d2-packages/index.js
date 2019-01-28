536afc453c7feac8a623f38fb4a08c6b2b50b502
Austin McGee <>
2019-01-28 17:38:56 +0000

    chore: remove d2-packages for now

--- packages/d2-packages/index.js
+++
@@ -1,9 +0,0 @@
-const { namespace } = require('@dhis2/cli-helpers-engine')
-
-const command = namespace('packages', {
-    desc: 'Manage DHIS2 packages',
-    aliases: 'pkg',
-    builder: require('./lib/cmds'),
-})
-
-module.exports = command
--- packages/d2/src/index.js
+++ packages/d2/src/index.js
@@ -6,7 +6,6 @@ const command = namespace('d2', {
         yargs.commandDir('commands')
         yargs.command(require('@dhis2/cli-app'))
         yargs.command(require('@dhis2/cli-cluster'))
-        yargs.command(require('@dhis2/cli-packages'))
         yargs.command(require('@dhis2/cli-style'))
     },
 })
