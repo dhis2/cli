e4e4f53d4a561c1eabc81808bd23bf3d11ac5cfc
Viktor Varland <varl@vlv.io>
2019-03-21 16:06:59 +0100

    chore: cli-packages begone

--- packages/d2/src/index.js
+++ packages/d2/src/index.js
@@ -5,7 +5,6 @@ const command = namespace('d2', {
     builder: yargs => {
         yargs.commandDir('commands')
         yargs.command(require('@dhis2/cli-cluster'))
-        yargs.command(require('@dhis2/cli-packages'))
         yargs.command(require('@dhis2/cli-create'))
         yargs.command(require('@dhis2/cli-style'))
         yargs.command(require('@dhis2/cli-utils'))
