dde292ef616b46837b17103ccbd697eef9505852
Viktor Varland <viktor@vardevs.se>
2019-03-21 09:56:53 +0100

    chore: use reporter and reformat

--- packages/d2-app/src/commands/scripts/release.js
+++ packages/d2-app/src/commands/scripts/release.js
@@ -1,28 +1,35 @@
 const { reporter } = require('@dhis2/cli-helpers-engine')
 
-const semanticRelease = require('semantic-release');
+const semanticRelease = require('semantic-release')
 
 const options = {
-	branch: 'master',
-    version: 'v${version}'
+    branch: 'master',
+    version: 'v${version}',
 
-	plugins: [
-		'@semantic-release/commit-analyzer',
-		'@semantic-release/release-notes-generator',
+    plugins: [
+        '@semantic-release/commit-analyzer',
+        '@semantic-release/release-notes-generator',
 
-		['@semantic-release/changelog', {
-			changelogFile: 'CHANGELOG.md',
-		}],
+        [
+            '@semantic-release/changelog',
+            {
+                changelogFile: 'CHANGELOG.md',
+            },
+        ],
 
-		'@semantic-release/npm',
+        '@semantic-release/npm',
 
-		['@semantic-release/git', {
-			assets: ['CHANGELOG.md', 'package.json', 'yarn.lock'],
-			message: 'chore(release): cut ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
-		}],
+        [
+            '@semantic-release/git',
+            {
+                assets: ['CHANGELOG.md', 'package.json', 'yarn.lock'],
+                message:
+                    'chore(release): cut ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+            },
+        ],
 
         '@semantic-release/github',
-	],
+    ],
 }
 
 const handler = async ({ name }) => {
@@ -33,26 +40,34 @@ const handler = async ({ name }) => {
             GIT_AUTHOR_EMAIL: 'ci@dhis2.org',
             GIT_COMMITTER_NAME: '@dhis2-bot',
             GIT_COMMITTER_EMAIL: 'ci@dhis2.org',
-        }
+        },
     }
 
     try {
-        const result = await semanticRelease(options, config);
+        const result = await semanticRelease(options, config)
 
         if (result) {
-            const {lastRelease, commits, nextRelease, releases} = result;
+            const { lastRelease, commits, nextRelease, releases } = result
 
-            reporter.info(`Published ${nextRelease.type} release version ${nextRelease.version} containing ${commits.length} commits.`);
+            reporter.info(
+                `Published ${nextRelease.type} release version ${
+                    nextRelease.version
+                } containing ${commits.length} commits.`
+            )
 
             if (lastRelease.version) {
-                reporter.info(`The last release was "${lastRelease.version}".`);
+                reporter.info(`The last release was "${lastRelease.version}".`)
             }
 
             for (const release of releases) {
-                reporter.info(`The release was published with plugin "${release.pluginName}".`);
+                reporter.info(
+                    `The release was published with plugin "${
+                        release.pluginName
+                    }".`
+                )
             }
         } else {
-            reporter.info('No release published.');
+            reporter.info('No release published.')
         }
     } catch (err) {
         reporter.error('The automated release failed with %O', err)
