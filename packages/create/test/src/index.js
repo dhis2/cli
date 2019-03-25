fb0df1ab4d433b758891f355bb5bf5a4ab8e0acc
Austin McGee <947888+amcgee@users.noreply.github.com>
2019-03-25 15:32:03 +0100

    chore: remove accidentally-committed test directory

--- packages/create/test/src/index.js
+++
@@ -1,6 +0,0 @@
-const { namespace } = require('@dhis2/cli-helpers-engine')
-
-module.exports = namespace('test', {
-    description: 'Testing...',
-    builder: yargs => yargs.commandDir('./commands'),
-})
