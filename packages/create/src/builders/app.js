a3c8d751d95cb189154fedf0e82828bb598c0f17
Austin McGee <947888+amcgee@users.noreply.github.com>
2019-03-28 14:18:52 +0100

    fix: upgrade @dhis2/cli-helpers-engine and @dhis2/cli-style

--- packages/create/src/builders/app.js
+++ packages/create/src/builders/app.js
@@ -5,11 +5,15 @@ const chalk = require('chalk')
 
 const buildApp = async ({ name, ...argv }) => {
     const data = { basename: name || 'myApp' }
-    reporter.info(`Creating DHIS2 application ${chalk.bold(data.basename)}...`)
-    // const data = await cliBuilder({ name, ...argv })
+    reporter.info(
+        `Hold your horses!  DHIS2 application bootstrapping with ${chalk.bold(
+            'd2 create app'
+        )} is coming soon...`
+    )
+    // reporter.info(`Creating DHIS2 application ${chalk.bold(data.basename)}...`)
 
-    const dest = path.join(process.cwd(), data.basename)
-    await installTemplate('app', dest, data)
+    // const dest = path.join(process.cwd(), data.basename)
+    // await installTemplate('app', dest, data)
 }
 
 module.exports = buildApp
