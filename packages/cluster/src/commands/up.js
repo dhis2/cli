e56b400d84f2c2722848aef9025612820e6ba779
Viktor Varland <viktor@vardevs.se>
2019-05-09 08:47:12 +0200

    feat: add context argument to cluster up

--- packages/cluster/src/commands/up.js
+++ packages/cluster/src/commands/up.js
@@ -1,5 +1,6 @@
 const chalk = require('chalk')
 const path = require('path')
+const { readFileSync, writeFileSync } = require('fs')
 const { reporter, exec, tryCatchAsync } = require('@dhis2/cli-helpers-engine')
 const {
     initDockerComposeCache,
@@ -8,12 +9,21 @@ const {
 } = require('../common')
 const { seed: doSeed } = require('../db')
 
-const run = async function({ v, port, seed, seedFile, update, ...argv }) {
+const run = async function({
+    v,
+    port,
+    seed,
+    seedFile,
+    update,
+    context,
+    ...argv
+}) {
     const cacheLocation = await initDockerComposeCache({
         cache: argv.getCache(),
         dockerComposeRepository: argv.cluster.dockerComposeRepository,
         force: update,
     })
+
     if (!cacheLocation) {
         reporter.error('Failed to initialize cache...')
         process.exit(1)
@@ -21,6 +31,28 @@ const run = async function({ v, port, seed, seedFile, update, ...argv }) {
 
     if (seed || seedFile) {
         await doSeed({ cacheLocation, v, path: seedFile, update, ...argv })
+    }
+
+    if (context) {
+        try {
+            reporter.info(
+                `Setting Tomcat context path to ${chalk.cyan(context)}`
+            )
+            const serverxml = readFileSync(
+                path.join(cacheLocation, 'config', 'tomcat-server.xml')
+            ).replace(
+                '{REPLACE_WITH_CONTEXT}',
+                `<Context path="/${context}" docBase="ROOT/" />`
+            )
+
+            writeFileSync(
+                path.join(cacheLocation, 'config', 'tomcat-server.xml'),
+                serverxml
+            )
+        } catch (e) {
+            reporter.error('Failed to modifiy server.xml', e)
+            process.exit(1)
+        }
     }
 
     reporter.info(`Spinning up cluster version ${chalk.cyan(v)}`)
@@ -54,6 +86,12 @@ module.exports = {
     desc: 'Spin up a new cluster',
     aliases: 'u',
     builder: {
+        context: {
+            alias: 'c',
+            desc: 'Set the Tomcat context path',
+            type: 'string',
+            default: '',
+        },
         port: {
             alias: 'p',
             desc: 'Specify the port on which to expose the DHIS2 instance',
