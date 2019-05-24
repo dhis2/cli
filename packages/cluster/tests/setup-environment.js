c6fbb5c4d33a97e56229eca29c4ef9f863904a7b
Viktor Varland <viktor@vardevs.se>
2019-05-24 17:27:30 +0200

    chore: add more tests for environment

--- packages/cluster/tests/setup-environment.js
+++ packages/cluster/tests/setup-environment.js
@@ -24,3 +24,32 @@ test('build runtime environment based on defaults', function(t) {
 
     t.deepEqual(actual, expected, 'default environment')
 })
+
+test('build runtime environment based on args', function(t) {
+    t.plan(1)
+
+    const argv = {
+        name: 'dev',
+        customContext: true,
+        dhis2Version: '2.33',
+        dbVersion: '2.32',
+        channel: 'canary',
+        variant: ['jetty', 'slackware'],
+        port: 8233,
+    }
+    const cache = {}
+    const config = {}
+
+    const actual = makeEnvironment(argv, cache, config)
+
+    const expected = {
+        DHIS2_CORE_NAME: 'dev',
+        DHIS2_CORE_CONTEXT_PATH: '/dev',
+        DHIS2_CORE_IMAGE: 'dhis2/core-canary:2.33-jetty-slackware',
+        DHIS2_CORE_VERSION: '2.33',
+        DHIS2_CORE_DB_VERSION: '2.32',
+        DHIS2_CORE_PORT: 8233,
+    }
+
+    t.deepEqual(actual, expected, 'default environment')
+})
