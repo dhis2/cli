2616d8e57eebbf8d3069ae53795c551126c8dbee
Viktor Varland <viktor@vardevs.se>
2019-06-06 11:15:08 +0200

    fix: use dhis2/docker-compose instead of amcgee/dhis2-backend as default

--- packages/cluster/src/defaults.js
+++ packages/cluster/src/defaults.js
@@ -8,7 +8,7 @@ module.exports = {
     update: false,
     seed: false,
     dockerComposeRepository:
-        'https://github.com/amcgee/dhis2-backend/archive/master.tar.gz',
+        'https://github.com/dhis2/docker-compose/archive/master.tar.gz',
     demoDatabaseURL:
         'https://github.com/dhis2/dhis2-demo-db/blob/master/sierra-leone/{version}/dhis2-db-sierra-leone.sql.gz?raw=true',
 }
