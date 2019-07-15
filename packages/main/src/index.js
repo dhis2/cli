333b23dc2e42c5af0be6c6550560b0a366531680
Viktor Varland <varl@vlv.io>
2019-07-15 12:55:20 +0200

    fix: update cli-style to match new version

--- packages/main/src/index.js
+++ packages/main/src/index.js
@@ -6,7 +6,7 @@ const command = namespace('d2', {
         yargs.command(require('@dhis2/cli-app'))
         yargs.command(require('@dhis2/cli-cluster'))
         yargs.command(require('@dhis2/cli-create'))
-        yargs.command(require('@dhis2/cli-style'))
+        yargs.command(require('@dhis2/cli-style').command)
         yargs.command(require('@dhis2/cli-utils'))
         yargs.commandDir('commands')
     },
