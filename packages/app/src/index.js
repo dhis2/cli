aed7f80dff98e5b364681b9acb770a9701323bd4
Austin McGee <947888+amcgee@users.noreply.github.com>
2019-08-29 17:19:21 +0200

    fix: don't stat an empty directory

--- packages/app/src/index.js
+++ packages/app/src/index.js
@@ -4,6 +4,5 @@ module.exports = namespace('app', {
     description: 'Front-end application and library commands',
     builder: yargs => {
         yargs.command(require('@dhis2/cli-app-scripts'))
-        yargs.commandDir('./commands')
     },
 })
