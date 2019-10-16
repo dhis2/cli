ef6b38506f5340bd65c97b7af59f60fc414635ba
Viktor Varland <viktor@dhis2.org>
2019-10-16 18:54:47 +0200

    feat: allow defer-release keyword in commit messages (#161)
    * feat: allow defer-release keyword in commit messages
    
    * fix: halt release process with exit code 0 when defer
    
    * refactor(release): allow space or dash in tag

---
+++ packages/utils/src/support/semantic-release-defer-release.js
@@ -0,0 +1,16 @@
+const SemanticReleaseError = require('@semantic-release/error')
+
+const analyzeCommits = (config = {}, context) => {
+    const { logger, commits } = context
+
+    const { message, commit } = commits[0]
+    const defer = /\[defer[ -]release\]/gi
+
+    if (message.match(defer)) {
+        logger.warn(`This release has been deferred by commit ${commit.short}`)
+        logger.complete('Halting release process...')
+        process.exit(0)
+    }
+}
+
+module.exports = { analyzeCommits }
